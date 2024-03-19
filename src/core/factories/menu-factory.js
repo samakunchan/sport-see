aa1fbe083bd03b6a4f3f9ccecf0899c7e9bd3e0b
samakunchan <badgehades@hotmail.com>
2024-03-19 14:56:06 +0100

    fix: path

--- src/core/factories/menu-factory.js
+++ src/core/factories/menu-factory.js
@@ -1,21 +1,17 @@
-import {SidebarMenuModel} from '../models/sidebar-menu-model';
 import {ToolbarMenuModel} from '../models/toolbar-menu-model';
 
 /**
  * Crée une instance de menu en fonction des données fournies.
  * @class
  * @param {Object} datas - Les données du menu.
- * @returns {ToolbarMenuModel|SidebarMenuModel} Une instance de ToolbarMenuModel ou SidebarMenuModel.
+ * @returns {ToolbarMenuModel} Une instance de ToolbarMenuModel ou SidebarMenuModel.
  * @throws {Error} Si le format des données est incorrect.
  */
 export class MenuFactory {
   constructor(datas) {
-    const { label, icone } = datas;
+    const { label } = datas;
     if (label !== undefined) {
       return new ToolbarMenuModel(datas);
-    }
-    if (icone !== undefined) {
-      return new SidebarMenuModel(datas);
     }
     throw new Error('Format incorrect pour le menu factory');
   }

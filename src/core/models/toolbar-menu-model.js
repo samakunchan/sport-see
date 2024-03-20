b258c90bc11ab50a666531db570265b8d7e7d6c9
samakunchan <badgehades@hotmail.com>
2024-03-20 15:47:54 +0100

    docs: models
    - Documentation des class models

--- src/core/models/toolbar-menu-model.js
+++ src/core/models/toolbar-menu-model.js
@@ -1,7 +1,18 @@
 import '../utils/utils';
 import { MenuBase } from './bases/menu-base';
 
+/**
+ * Crée une instance de menu de la sidebar hérité de {MenuBase}.
+ * @class
+ * @param {{label: string}} menu - Les données du menu.
+ * @returns {ToolbarMenuModel} Une instance de ToolbarMenuModel.
+ */
 export class ToolbarMenuModel extends MenuBase {
+  /**
+   * Crée une instance de ToolbarMenuModel.
+   * @constructor
+   * @param {{label: string}} menu - Les données du menu.
+   */
   constructor(menu) {
     super(menu);
     const { label } = menu;
